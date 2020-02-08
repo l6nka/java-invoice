2a75ce67fc0bc32848cedac3d79b710b3cf157d5
Lena Grabowska <lenka.grabowska@gmail.com>
2020-02-08 18:17:53 +0100

    fix testEmptyInvoiceHasEmptyTaxAmount

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,26 +2,42 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
-
+	
+	private Map<Product, Integer> products = new HashMap<>();
+	
 	public void addProduct(Product product) {
-		// TODO: implement
+		this.addProduct(product,1);
 	}
 
+
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		this.products.put(product, quantity);
+		
 	}
 
 	public BigDecimal getNetPrice() {
-		return BigDecimal.ZERO;
+		BigDecimal sum = BigDecimal.ZERO;
+		for (Product product: this.products.keySet()) {
+			Integer quantity = this.products.get(product);
+			sum = sum.add(product.getPrice().multiply(new BigDecimal(quantity)));
+		}
+		return sum;
 	}
 
 	public BigDecimal getTax() {
-		return BigDecimal.ZERO;	}
+		BigDecimal sum = BigDecimal.ZERO;
+		for (Product product: this.products.keySet()) {
+			Integer quantity = this.products.get(product);
+			sum = sum.add(product.getPriceWithTax().multiply(new BigDecimal(quantity)));
+		}
+		return sum;
+	}
 	
 
 	public BigDecimal getGrossPrice() {
